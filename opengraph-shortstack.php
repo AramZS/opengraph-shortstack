2180c479400b9af1062ee7d5c381a101d4de38f9
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-06-12 16:51:56 -0400

    Initial Commit

---
+++ opengraph-shortstack.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Plugin Name: Open Graph Shortstack
+Plugin URI: http://aramzs.me/og-shortstack
+Description: This plugin does some nifty stuff with open graph. 
+Version: 0.2
+Author: Aram Zucker-Scharff
+Author URI: http://aramzs.me
+License: GPL2
+*/
+
+/*  Copyright 2012  Aram Zucker-Scharff  (email : azuckers@gmu.edu)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+?>
